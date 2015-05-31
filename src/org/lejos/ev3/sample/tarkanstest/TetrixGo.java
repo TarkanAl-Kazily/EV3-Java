9661c7d2ae33c9f4dccea4956d5b46eb2bd6cbcd
TarkanAl-Kazily <tarkucar@mac.com>
2015-05-30 22:45:50 -0700

    Modified TetrixGo with easier to use methods

--- src/org/lejos/ev3/sample/tarkanstest/TetrixGo.java
+++ src/org/lejos/ev3/sample/tarkanstest/TetrixGo.java
@@ -1,7 +1,12 @@
 package org.lejos.ev3.sample.tarkanstest;
 
+import lejos.hardware.BrickFinder;
 import lejos.hardware.device.tetrix.TetrixControllerFactory;
 import lejos.hardware.device.tetrix.TetrixMotorController;
+import lejos.hardware.device.tetrix.TetrixServo;
+import lejos.hardware.device.tetrix.TetrixServoController;
+import lejos.hardware.lcd.Font;
+import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
@@ -22,8 +27,10 @@ public class TetrixGo {
 	static DCMotor motor1;
 	static DCMotor motor2;
 	static TetrixMotorController motorController;
+	static TetrixServoController servoController;
 	static TetrixControllerFactory factory;
-	
+	static final boolean LEFT = true;
+	static final boolean RIGHT = false;
 	static SensorModes buttonSensor;
 	static SampleProvider pressed;
 	
@@ -32,30 +39,89 @@ public class TetrixGo {
 	
 	static float[] sample;
 	
+	static final int MICROSECLOW_DEFAULT = 750;
+	static final int MICROSECHIGH_DEFAULT = 2250;
+	static final int TRAVELRANGE_DEFAULT = 200;
 	
 	public static void main(String[] args) {
-		whileDistanceAway(.1);
+		initializeControllers();
+		initializeMotors();
+		initializeUltrasonicSensor();
+		RegulatedMotor m = new EV3LargeRegulatedMotor(MotorPort.A);
+		setPower(50);
+		moveForward();
+		m.forward();
+		while (distanceRead() >= .3) {
+			Delay.msDelay(5);
+		}
+		stop();
+		Delay.msDelay(100);
+		moveBackward();
+		m.backward();
+		while (distanceRead() <=.7) {
+			Delay.msDelay(5);
+		}
+		m.stop();
+		stop();
+		
 	}
-	
-	public static void moveForward(int duration) {
+	public static void turn(boolean direction){
+		if(LEFT==direction){
+			motor1.forward();
+			motor2.forward();
+		}else{
+			motor1.backward();
+			motor2.backward();
+		}
+	}
+	/**
+	 * Always run this first, before initializing anything else
+	 */
+	public static void initializeControllers() {
 		factory = new TetrixControllerFactory(SensorPort.S1);
 		motorController = factory.newMotorController();
-		motor1 = motorController.getBasicMotor(TetrixMotorController.MOTOR_1);
-		motor2 = motorController.getBasicMotor(TetrixMotorController.MOTOR_2);
-		setPower(50);
-		forward();
-		Delay.msDelay(duration);
-		stop();
+		servoController = factory.newServoController();
+	}
+	
+	public static void moveForward() {
+		motor1.backward();
+		motor2.forward();
+	}
+	
+	public static void moveBackward() {
+		motor1.forward();
+		motor2.backward();
+	}
+	
+	/**
+	 * Run this before using the tetrix motors
+	 */
+	public static void initializeMotors() {
+		if (motorController != null) {
+			motor1 = motorController.getBasicMotor(TetrixMotorController.MOTOR_1);
+			motor2 = motorController.getBasicMotor(TetrixMotorController.MOTOR_2);
+		}
+	}
+	/**
+	 * Run this to get a new TetrixServo object
+	 * @param servoNumber the port the servo is connected to
+	 * @return the servo object
+	 */
+	public static TetrixServo initializeServo(int servoNumber) {
+		if (servoController != null) {
+			return servoController.getServo(servoNumber);
+		} else {
+			return null;
+		}
+	}
+	
+	public static void servoDefaults(TetrixServo servo) {
+		servo.setRange(MICROSECLOW_DEFAULT, MICROSECHIGH_DEFAULT, TRAVELRANGE_DEFAULT);
 	}
 	
 	public static void setPower(int power) {
-		motor1.setPower(power);
+		motor1.setPower(power-10);
 		motor2.setPower(power);
-	}
-	
-	public static void forward() {
-		motor1.forward();
-		motor2.forward();
 	}
 
 	public static void stop() {
@@ -76,6 +142,12 @@ public class TetrixGo {
 			}
 			Delay.msDelay(100);
 		}
+	}
+	
+	public static void initializeUltrasonicSensor() {
+		ultrasonicSensor = new EV3UltrasonicSensor(ULTRASONIC_SENSOR_PORT);
+		distance = ultrasonicSensor.getMode("Distance");
+		sample = new float[distance.sampleSize()];
 	}
 	
 	public static void whileDistanceAway(double dist) {
